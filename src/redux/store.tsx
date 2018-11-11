60a4c7fe229ab1169582df307947ca4b33a93a34
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-11-11 20:40:53 +0200

    Update redux-render-prop

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,13 @@
 import {createActionCreators, createReducerFunction} from "immer-reducer";
 import {bindActionCreators, createStore} from "redux";
-import {makeComponentCreator} from "redux-render-prop";
+import {makeConnector} from "redux-render-prop";
 
 import {TodoReducer} from "./actions";
 import {initialState, Selectors, State} from "./state";
 
 export const ActionCreators = createActionCreators(TodoReducer);
 
-export const createTodoConnect = makeComponentCreator({
+export const createTodoConnect = makeConnector({
     prepareState: (state: State) => new Selectors(state),
     prepareActions: dispatch => bindActionCreators(ActionCreators, dispatch),
 });
